afedbb665e5c21b3637c8bcd902e0f20af5b3c66
Oscar G <murdexchannel@gmail.com>
2023-09-11 23:31:22 -0400

    saving form validation progress

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,32 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Contact.css'
+import { FormInput } from './FormInput'
+
+
 export const Contact = () => {
+    const [formValues, setValues] = useState({
+        firstName:"",
+        lastName:"",
+        email:"",
+    });
+
+    const inputs = [
+        {
+            id:1,
+            name:"firstName",
+            type:"text",
+            placeholder:"First Name",
+            errorMessage:"First name length minimum is 2 and no special characters.",
+            label:"First Name *"
+        },
+        {
+            id:2,
+            name:"lastName",
+            type:"text",
+            placeholder:"Last Name",
+            errorMessage:"Last name length minimum is 2 and no special characters.",
+            label:"Last Name *"
+        },
+        {
+            id:3,
+            name:"email",
+            type:"email",
+            placeholder:"Email",
+            errorMessage:"Please enter a valid email.",
+            label:"Email *"
+        }
+
+    ]
+
+    const onChange = (e) => {
+        setValues({...formValues, [e.target.name]: e.target.value})
+        console.log(e.target.name);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
 
   return (
     <div className='contact-container' id='contact'>
 
         <h1>Let's Connect!</h1>
-        <form action='#' autoComplete='on'>
-            <label htmlFor='firstName'> Name* </label>
-            <input 
-            type='text' 
-            id='firstName' 
-            name='firstName' 
-            required></input>
+        <form action='#' autoComplete='on' onSubmit={handleSubmit}>
 
-            <label htmlFor='lastName' >Last Name* </label>
-            <input 
-            type='text' 
-            id='lastName' 
-            name='lastName' 
-            required ></input>
-
-            <label htmlFor='email'>Email* </label>
-            <input 
-            type='email' 
-            id='email' 
-            name='email' 
-            required></input>
+            {inputs.map((input) => (
+                <FormInput key={input.id} {...input} value={formValues[input.name]} onChange={onChange}/>
+            )
+            )}
 
             <label htmlFor='reason'>Reason: </label>
             <select id='reason'>
@@ -41,7 +71,7 @@ export const Contact = () => {
             <textarea 
             id='inquiry' 
             rows='10'
-            name='inquiry' />            
+            name='inquiry'/>            
 
             <input type='submit' value='Send' /> 
         </form>
--- src/Components/FormInput.js
+++ src/Components/FormInput.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import './Contact.css'
 
-export const FormInput = () => {
+export const FormInput = (props) => {
+
+  const {label, onChange, id, errorMessage, ...inputProps} = props;
+
   return (
     <div className='formInput'>
-        <label htmlFor={props.name}></label>
-        <input type={props.input} placeholder={props.placeholder} ></input>
+        <label htmlFor={props.name}>{label}</label>
+        <input {...inputProps} onChange={onChange}></input>
+        <span>{errorMessage}</span>
     </div>
   )
 }
