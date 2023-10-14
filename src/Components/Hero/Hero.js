88cfc6f3bcf356947e15c230b70affa8ce23e985
Oscar G <murdexchannel@gmail.com>
2023-10-14 00:19:18 -0400

    responsive desing has been modified for screens below 1140px

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import 'Components/Hero/Hero.css'
 import heroPicture from 'Assets/oscar-hero.jpg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faSquareFacebook, faSquareGithub} from '@fortawesome/free-brands-svg-icons'
 
 export const Hero = () => {
   return (
@@ -9,6 +11,22 @@ export const Hero = () => {
           <div className='image-container'>
               <img src={heroPicture} alt='Oscars-Picture'/>
               <h1 className='name-heading'> Oscar Gomez </h1>
+              <div className='hero-socials'>
+                <a href='https://github.com/oscargomez15' target='__blank'>
+                  <FontAwesomeIcon icon={faSquareGithub} size='2xl'/>
+                </a>
+
+                <a href='https://www.linkedin.com/in/oscar-gomez-77b472195/' target='__blank'>
+                  <FontAwesomeIcon icon={faLinkedin} size='2xl' />
+                </a>        
+                
+                <a href='https://www.facebook.com/oscargomez1998' target='__blank'>
+                  <FontAwesomeIcon icon={faSquareFacebook} size='2xl'/>
+                </a>
+              </div>
+              <p>Hi, there and welcome to my portfolio! I'm a front end developer and a Cloud Enthusiast
+                  who's willing to help your business grow and adapt to a constantly changing tech enviorment.<br/></p>
+                  <input type='submit' value='Download Resume'/>
             </div>
             <div className='hero-text'>
                 <h1>Hello there,</h1>
